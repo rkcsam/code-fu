0d3725c523262212b3d14a292d8b7df15f8f4b16
rohit <rohit.chourasia@cerner.com>
2015-11-26 14:17:19 +0530

    Completed Code

--- code_fu_1.rb
+++ code_fu_1.rb
@@ -1,10 +1,22 @@
 # Print the second highest element within a list of numbers input from the command line
- puts "Print the List"
- elementList = [gets]
-puts "Print the List #{elementList}"
-elementList.each do |i|
-  if i < 10
-    puts "inputs are #{i}"
-   end
-
+puts "Enter the number List"
+elementList = gets
+list=elementList.split.map(&:to_i)
+  puts "Print the List #{list}"
+ArrayLength=list.length
+for j in 1..(ArrayLength - 1)
+    highest = list[j]
+    i = j - 1
+    while i >= 0 and list[i] > highest
+      list[i+1] = list[i]
+      i = i - 1
+    end
+    list[i+1] = highest
+  end
+  list = list.uniq
+  list = list.reverse
+  if list.length < 2
+    puts "First highest number is #{list[0]}!"
+  else
+    puts "Second highest number is #{list[1]}"
 end
